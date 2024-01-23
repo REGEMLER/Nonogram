f4876d6ab20c4f712e63fcd02d4c85ef887b88e9
Yar Night <yaroslav239@gmail.com>
2024-01-23 21:01:40 +0300

    refactor: replace picture model to picture matrix, add new data attributes, rewrite win function

--- nonograms/JS/Models/Easy/cross.js
+++ nonograms/JS/Models/Easy/cross.js
@@ -192,3 +192,24 @@ export const crossModelHelper = [
         text: 2
     },
 ];
+
+export const cross = [
+    false, true, true, true, false,
+    true, true, false, true, true,
+    true, false, false, false, true,
+    true, true, false, true, true,
+    false, true, true, true, false,
+]
+
+export const crossTop = [
+    "", 2, 1, 2, "",
+    3, 2, 1, 2, 3
+]
+
+export const crossLeft = [
+    "", 3,
+    2, 2,
+    1, 1,
+    2, 2,
+    "", 3
+]
--- nonograms/JS/_index.js
+++ nonograms/JS/_index.js
@@ -1,5 +1,5 @@
 import {towerModelHelper, towerModel} from "./Models/Easy/tower.js";
-import {crossModelHelper, crossModel, cross, crossLeft, crossTop} from "./Models/Easy/cross.js";
+import {crossModelHelper, crossModel} from "./Models/Easy/cross.js";
 import {skullModelHelper, skullModel} from "./Models/Easy/skull.js";
 import {batModelHelper, batModel} from "./Models/Easy/bat.js";
 import {treeModelHelper, treeModel} from "./Models/Easy/tree.js";
@@ -9,20 +9,8 @@ import {musicModelHelper, musicModel} from "./Models/Medium/music.js";
 import {mouseModelHelper, mouseModel} from "./Models/Medium/mouse.js";
 import {cherryModelHelper, cherryModel} from "./Models/Medium/cherry.js";
 
-const model = [...cross];
-const topNumbers = [...crossTop];
-const leftNumbers = [...crossLeft];
-
-function fullField() {
-    const topElements = [...document.querySelectorAll("[data-cell='top']")];
-    topElements.forEach((item,index) => {
-        item.textContent = topNumbers[index];
-    });
-    const leftElements = [...document.querySelectorAll("[data-cell='left']")];
-    leftElements.forEach((item,index) => {
-        item.textContent = leftNumbers[index];
-    });
-}
+const model = [...snailModel];
+const modelHelper = [...snailModelHelper];
 
 function createField(numberHelpers, size) {
     const field = document.querySelector(".field");
@@ -40,42 +28,52 @@ function createField(numberHelpers, size) {
         for (let j = 1; j <= size; j++) {
             const cell = document.createElement("DIV");
             cell.classList.add("cell");
-
             if (i>numberHelpers && j>numberHelpers) {
-                cell.dataset.cell = "cell";
-            } 
-            if (i>=numberHelpers && j <=numberHelpers) {
-                cell.dataset.cell = "left";
-                cell.classList.add("clue");
-            } 
-            if (i<=numberHelpers && j>=numberHelpers) {
-                cell.dataset.cell = "top";
-                cell.classList.add("clue");
-            } 
-            if (i < numberHelpers + 1 && j < numberHelpers + 1) {
-                cell.dataset.cell = "empty";
-                cell.classList.add("cell_empty");
+                cell.id = `${i - numberHelpers}-${j - numberHelpers}`;
+                cell.dataset.isCell = true;
+                cell.textContent = cell.id;
+            } else {
+                cell.id = `${i}-${j}h`;
+                cell.dataset.isCell = false;
+                const id = modelHelper.find(item => item.id === cell.id);
+                if(id) {
+                    cell.textContent = id.text;
+                }
+                // cell.textContent = cell.id;
             }
+
             if (j === numberHelpers) {
                 cell.classList.add("cell_helper");
             }
             if ((j - numberHelpers) % 5 === 0 && j !== numberHelpers && j !== size) {
                 cell.classList.add("cell_five");
             }
+            if (i < numberHelpers + 1 && j < numberHelpers + 1) {
+                cell.classList.add("cell_empty");
+            }
             row.append(cell);
         }
         field.append(row);
-        fullField();
     }
 }
-createField(topNumbers.length / 5, topNumbers.length / 5 + 5);
+createField(4, 14);
 
-function checkWin() {
-    const cellElements = [...document.querySelectorAll("[data-cell='cell']")];
-    const cellElementsMatrix = cellElements.map( item => item.classList.contains("cell_click"));
-    return cellElementsMatrix.every((item, index) => {
-        return item === model[index];
-    })
+
+function checkWin(arr) {
+    return arr.every(item => item.isGuesed);
+}
+
+function checkGuesed(model, arr) {
+    model.map((item) => {
+        const element = arr.find(i => i.id === item.id);
+        if(element.classList.contains("cell_click") && item.isFull ||
+        !element.classList.contains("cell_click") && !item.isFull) {
+            item.isGuesed = true;
+        } else {
+            item.isGuesed = false;
+        }
+    });
+    // console.log(model);
 }
 
 function counter() {
@@ -85,7 +83,7 @@ function counter() {
 }
 
 function onClick(event) {
-    if(!event.target.dataset.cell || event.target.dataset.cell !== "cell") return;
+    if(!event.target.dataset.isCell || event.target.dataset.isCell === "false") return;
     counter();
 
     if (event.target.classList.contains("cell_click")) {
@@ -93,11 +91,13 @@ function onClick(event) {
     } else {
         event.target.classList.add("cell_click");
     };
-    if(checkWin()) console.log("Win")
+
+    checkGuesed(model, [...document.querySelectorAll("[data-is-cell='true']")]);
+    if(checkWin(model)) console.log("Win")
 }
 
 function onContextMenu(event) {
-    if(!event.target.dataset.cell || event.target.dataset.cell !== "cell") return;
+    if(!event.target.dataset.isCell || event.target.dataset.isCell === "false") return;
     event.preventDefault();
 
     if (event.target.textContent === "X") {
@@ -105,6 +105,9 @@ function onContextMenu(event) {
     } else {
         event.target.textContent = "X";
     }
+
+    checkGuesed(model, [...document.querySelectorAll("[data-is-cell='true']")]);
+    if(checkWin(model)) console.log("Win")
 }
 
 document.body.addEventListener("click", onClick);
